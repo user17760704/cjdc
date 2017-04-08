363e96ebf72bb64ca382cae876bd5556264d30f5
Patch <Patch@192.168.78.128>
2017-04-08 16:12:04 -0500

    Changes to For Loop class

--- ForLoop/src/ForLoop.java
+++ ForLoop/src/ForLoop.java
@@ -5,7 +5,16 @@ public class ForLoop
 	public static void main(String[] args)
 	{
 	
-		System.out.println("");
+		System.out.println("10000 at 2% interest: " + calculateInterest(10000, 2));
+		System.out.println("10000 at 3% interest: " + calculateInterest(10000, 3));
+		System.out.println("10000 at 4% interest: " + calculateInterest(10000, 4));
+		System.out.println("10000 at 5% interest: " + calculateInterest(10000, 5));
+		
+		System.out.println("**************************\n");
+		for (int i = 2; i <= 8; i++)
+		{
+			System.out.println("10000 at " + i + "% interest: " + calculateInterest(10000, i));
+		}
 	}
 	
 	public static double calculateInterest(double amount, double interestRate)
