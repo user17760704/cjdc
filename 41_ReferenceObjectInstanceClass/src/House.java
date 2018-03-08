17abf4a0e8375329bd7ea645437050c8fa76c939
Ravi Chandra <ravi@powercosts.com>
2018-03-08 13:51:38 -0600

    Classes, reference, instance and objects

---
+++ 41_ReferenceObjectInstanceClass/src/House.java
@@ -0,0 +1,21 @@
+
+public class House
+{
+	private String _color;
+
+	public House(String color)
+	{
+		super();
+		this._color = color;
+	}
+
+	public String getColor()
+	{
+		return _color;
+	}
+
+	public void setColor(String color)
+	{
+		this._color = color;
+	}
+}
