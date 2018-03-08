836125f2283634a16a4b5d1d3969180d871f9aca
Ravi Chandra <ravi@powercosts.com>
2018-03-08 14:46:44 -0600

    changes to references

--- 41_ReferenceObjectInstanceClass/src/Main.java
+++ 41_ReferenceObjectInstanceClass/src/Main.java
@@ -6,6 +6,13 @@ public class Main
 	{
 		
 		House blueHouse = new House("blue");
+		House anotherHouse = blueHouse;
+		
+		anotherHouse.setColor("red");
+		System.out.println("blueHouse color: " + blueHouse.getColor());
+		System.out.println("anotherHouse color: " + anotherHouse.getColor());
+		
+		House greenHouse = new House("green");
 
 	}
 
